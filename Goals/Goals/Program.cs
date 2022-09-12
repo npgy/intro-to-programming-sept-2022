5b39b1d817a4dd9b54e0e124da0dbf41fbffc137
npgy <me@np.gy>
2022-09-12 17:01:29 -0400

    initial

---
+++ Goals/Goals/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your goal for today:");
+Console.WriteLine("--------------------------");
+string? goalUnprocessed = Console.ReadLine();
+string goalFinal = "";
+
+try
+{
+	goalFinal = goalUnprocessed?.Substring(0, 255);
+}
+catch (ArgumentOutOfRangeException)
+{
+	goalFinal = goalUnprocessed;
+}
+
+Console.WriteLine($"\n[For {DateTime.Now:D} Your Goal Is:]\n");
+Console.WriteLine($"\"{goalFinal}\"");
+
+/*using System.Windows.Forms.Application;
+
+Console.WriteLine(Application.LocalUserAppDataPath);*/
