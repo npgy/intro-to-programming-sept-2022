a7b788e053e7893e9a6d6ef04afc762d693026f4
npgy <me@np.gy>
2022-09-13 16:54:29 -0400

    string calculator completed up to step 4

---
+++ stringcalculator-tuesday/StringCalculator.cs
@@ -0,0 +1,61 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+    private char[] Delimeters = { ',', '\n' };
+    public int Add(string numbers)
+    {
+        string readDelimOpts = "";
+        int numStart = 0;
+        int sum = 0;
+        try
+        {
+            readDelimOpts = numbers[..3];
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            //Swallow!
+        }
+        finally
+        {
+            if (readDelimOpts.StartsWith("//"))
+            {
+                numStart = 2;
+                Delimeters = new char[] { readDelimOpts[2] };
+            }
+        }
+        string[] numberList = numbers.Substring(numStart,numbers.Length-numStart).Split(Delimeters);
+        foreach (string number in numberList)
+        {
+            if (!number.Equals(""))
+            {
+                sum += int.Parse(number);
+            } 
+        }
+        return sum;
+    }
+
+    public char[] GetDelimeters(string query)
+    {
+        string readDelimOpts = "";
+
+        try
+        {
+            readDelimOpts = query[..3];
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            //Swallow!
+        }
+        finally
+        {
+            if (readDelimOpts.StartsWith("//"))
+            {
+                Delimeters = new char[] { readDelimOpts[2] };
+            }
+        }
+
+        return Delimeters;
+    }
+}
---
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -0,0 +1,185 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void SingleNumberReturnsItself()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("5");
+
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void TwoNumbersReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("5,8");
+
+        Assert.Equal(13, result);
+    }
+
+    [Fact]
+    public void ThreeNumbersReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("15,23,91");
+
+        Assert.Equal(129, result);
+    }
+
+    [Fact]
+    public void FourNumbersReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("15,23,91,2");
+
+        Assert.Equal(131, result);
+    }
+
+    [Fact]
+    public void FiveNumbersReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("69,15,23,91,2");
+
+        Assert.Equal(200, result);
+    }
+
+    [Fact]
+    public void SixNumbersReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("200,69,15,23,91,2");
+
+        Assert.Equal(400, result);
+    }
+
+    [Fact]
+    public void TwoNumbersWithNewLinesReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("4\n7");
+
+        Assert.Equal(11, result);
+    }
+
+    [Fact]
+    public void ThreeNumbersWithNewLinesReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("15\n23\n91");
+
+        Assert.Equal(129, result);
+    }
+
+    [Fact]
+    public void FourNumbersWithNewLinesReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("15\n23\n91\n2");
+
+        Assert.Equal(131, result);
+    }
+
+    [Fact]
+    public void FiveNumbersWithNewLinesReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("69\n15\n23\n91\n2");
+
+        Assert.Equal(200, result);
+    }
+
+    [Fact]
+    public void SixNumbersWithNewLinesReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("200\n69\n15\n23\n91\n2");
+
+        Assert.Equal(400, result);
+    }
+
+    [Fact]
+    public void FourNumbersWithMixedDelimsReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("15\n23,91\n2");
+
+        Assert.Equal(131, result);
+    }
+
+    [Fact]
+    public void FiveNumbersWithMixedDelimsReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("25,15\n23,91\n2");
+
+        Assert.Equal(156, result);
+    }
+
+    [Fact]
+    public void CustomerDelimiterIsSet()
+    {
+        var calculator = new StringCalculator();
+
+        char[] result = calculator.GetDelimeters("//;4;7");
+
+        Assert.Equal(';', result[0]);
+    }
+
+    [Fact]
+    public void TwoNumbersWithSemicolonDelimReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("//;4;7");
+
+        Assert.Equal(11, result);
+    }
+
+    [Fact]
+    public void FiveNumbersWithPlusDelimReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("//+25+15+23+91+2");
+
+        Assert.Equal(156, result);
+    }
+
+    [Fact]
+    public void FiveNumbersWithStarDelimReturnTheirSum()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("//*25*15*23*91*2");
+
+        Assert.Equal(156, result);
+    }
+}
