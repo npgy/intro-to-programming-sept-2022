2ed99a934be7577eba2b343a2454c11bdbff36c5
npgy <me@np.gy>
2022-09-14 14:23:30 -0400

    done with inheritance

--- Banking/Banking.Domain/BankAccount.cs
+++ Banking/Banking.Domain/BankAccount.cs
@@ -4,7 +4,7 @@ public class BankAccount
 {
     private decimal _balance = 5000M;
 
-    public void Deposit(decimal amountToDeposit)
+    public virtual void Deposit(decimal amountToDeposit)
     {
         _balance += amountToDeposit;
     }
---
+++ Banking/Banking.UnitTests/GoldAccountsGetBonusOnDeposit.cs
@@ -0,0 +1,16 @@
+﻿using Banking.Domain;
+namespace Banking.UnitTests;
+public class GoldAccountsGetBonusOnDeposit
+{
+    [Fact]
+    public void BonusAppliedOnDeposits()
+    {
+        var account = new GoldAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
+    }
+}
