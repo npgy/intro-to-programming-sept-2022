8be8525eebaf734a8f70fd130ba7abec01d3b142
npgy <me@np.gy>
2022-09-14 13:58:01 -0400

    create branch for customer loyalty

--- Banking/Banking.Domain/BankAccount.cs
+++ Banking/Banking.Domain/BankAccount.cs
@@ -3,10 +3,23 @@
 public class BankAccount
 {
     private decimal _balance = 5000M;
+    public string AccountType { get; set; } = "standard";
 
     public void Deposit(decimal amountToDeposit)
     {
-        _balance += amountToDeposit;
+        decimal bonus = GetBonusForDeposit(amountToDeposit);
+        _balance += amountToDeposit + bonus;
+    }
+
+    private decimal GetBonusForDeposit(decimal amountToDeposit)
+    {
+        decimal bonus = 0;
+        if (AccountType == "gold")
+        {
+            bonus = amountToDeposit * .10M;
+        }
+
+        return bonus;
     }
 
     public decimal GetBalance()
---
+++ Banking/Banking.UnitTests/GoldAccountsGetBonus.cs
@@ -0,0 +1,17 @@
+﻿using Banking.Domain;
+
+namespace Banking.UnitTests;
+public class GoldAccountsGetBonus
+{
+    [Fact]
+    public void GoldAccountsGetbBonusOnDeposit()
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 100M;
+
+        account.Deposit(amountToDeposit);
+
+        Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
+    }
+}
