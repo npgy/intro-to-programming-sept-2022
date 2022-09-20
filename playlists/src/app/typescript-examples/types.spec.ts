db8f73124c85770d8db6a0d75bdbc420c9594116
npgy <me@np.gy>
2022-09-20 12:16:36 -0400

    wrote some tests

---
+++ playlists/src/app/typescript-examples/types.spec.ts
@@ -0,0 +1,150 @@
+describe('basic types in TypeScript', () => {
+  it('explicitly typed variables', () => {
+    // initializing also implies data type
+    let x = 10;
+
+    expect(typeof x).toBe('number');
+
+    // this is implicitly typed to "any"
+    let y;
+
+    y = 10;
+    expect(typeof y).toBe('number');
+
+    y = 'Tacos';
+    expect(typeof y).toBe('string');
+
+    let name: number | string;
+
+    name = 1138;
+
+    name = 'Bob';
+
+    //name = () => {}
+  });
+  it('string weirdness', () => {
+    let name = 'Jeff';
+    let age = 53;
+    expect(name).toBe('Jeff');
+
+    let info = `This is ${name} and they are ${age} years old.`;
+
+    expect(info).toBe('This is Jeff and they are 53 years old.');
+  });
+  describe('User Defined Types', () => {
+    it('interfaces in typescript', () => {
+      interface Movie {
+        title: string;
+        director: string;
+        yearReleased: number;
+        cast?: { name: string; role: string }[];
+      }
+
+      const starwars: Movie = {
+        title: 'Star Wars',
+        director: 'Lucas',
+        yearReleased: 1978,
+        cast: [{ name: 'Mark Hammil', role: 'Luke Skywalker' }],
+      };
+
+      //starwars.yearReleased = 1977; // "Mutating the Object"
+
+      const updatedStarWars = { ...starwars, yearReleased: 1977 };
+
+      expect(starwars.yearReleased).toBe(1978);
+      expect(updatedStarWars.yearReleased).toBe(1977);
+    });
+    it('working with arrays', () => {
+      const friends = ['Sean', 'Zach', 'Jamie', 'Leeroy'];
+
+      const updatedFriends = ['Tara', ...friends, 'Billy'];
+      expect(updatedFriends).toEqual([
+        'Tara',
+        'Sean',
+        'Zach',
+        'Jamie',
+        'Leeroy',
+        'Billy',
+      ]);
+    });
+  });
+});
+
+describe('Functions', () => {
+  it('named functions', () => {
+    expect(formatName('Luke', 'Skywalker')).toBe('Skywalker, Luke');
+
+    expect(formatName('Han', 'Solo', 'D')).toBe('Solo, Han D.');
+
+    function formatName(
+      firstName: string,
+      lastName: string,
+      mi?: string
+    ): string {
+      let fullName = `${lastName}, ${firstName}`;
+      if (mi != undefined) {
+        fullName += ` ${mi}.`;
+      }
+      return fullName;
+    }
+  });
+  it('anonymous functions', () => {
+    const formatName = (firstName: string, lastName: string): string =>
+      `${lastName}, ${firstName}`;
+
+    expect(formatName('Luke', 'Skywalker')).toBe('Skywalker, Luke');
+  });
+
+  describe('Array Methods', () => {
+    it('has a foreach', () => {
+      let total = 0;
+      const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      numbers.forEach((e, i, c) => {
+        console.log({ e, i, c });
+        total += e;
+      });
+
+      expect(total).toBe(45);
+    });
+
+    describe('array methods that return a new array', () => {
+      it('has a map', () => {
+        // map is like "select" in LINQ
+        const numbers = [1, 2, 3, 4];
+        // if there is a place you want to go, it will get you there you know, it's the map.
+
+        const doubled = numbers.map((number) => number * 2);
+        expect(doubled).toEqual([2, 4, 6, 8]);
+      });
+      it('filter', () => {
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        const evens = numbers.filter((e) => e % 2 === 0);
+
+        expect(evens).toEqual([2, 4, 6, 8]);
+      });
+    });
+
+    describe('array methods that return a single (scalar) value', () => {
+      it('checking if every element in the array meets a criteria', () => {
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        const allEven = numbers.every((n) => n % 2 === 0);
+        expect(allEven).toBeFalse();
+
+        const someEven = numbers.some((n) => n % 2 === 0);
+        expect(someEven).toBeTrue();
+      });
+
+      it('the dreaded reduce', () => {
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let total = numbers.reduce((s, n) => s + n);
+        expect(total).toBe(45);
+
+        total = numbers.reduce((s, n) => s + n, 100);
+        expect(total).toBe(145);
+      });
+    });
+  });
+});
